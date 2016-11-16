fecd500bd54cd371d98abb4c51051d474ea125a2
Garrett <gn9012@gmail.com>
2016-11-15 21:58:04 -0600

    django settings.py email

--- dungeonomics/settings.py
+++ dungeonomics/settings.py
@@ -152,7 +152,14 @@ ACCOUNT_EMAIL_SUBJECT_PREFIX = ''
 
 DEFAULT_FROM_EMAIL = 'no-reply@dungeonomics.com'
 
+# email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'Dungeonomics <no-reply@dungeonomics.com>'
 
 
 # Static files (CSS, JavaScript, Images)
