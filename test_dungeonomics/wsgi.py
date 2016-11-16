3fc52f04d2554c44e6613b7ec2c00e3a2a837456
Garrett <gn9012@gmail.com>
2016-11-15 21:01:00 -0600

    test_dungeonomics > dungeonomics

--- test_dungeonomics/wsgi.py
+++ test_dungeonomics/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for test_dungeonomics project.
+WSGI config for dungeonomics project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_dungeonomics.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dungeonomics.settings")
 
 application = get_wsgi_application()
