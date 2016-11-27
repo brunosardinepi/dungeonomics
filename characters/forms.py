75d82033351a243fd5a7aab20286c87c73d4cd7e
Garrett <gn9012@gmail.com>
2016-11-27 15:50:30 -0600

    tinymce

--- characters/forms.py
+++ characters/forms.py
@@ -5,7 +5,15 @@ from . import models
 
 class TinyMCEForm(forms.ModelForm):
     class Media:
+        css = {
+            'all': (
+                '/static/css/autocomplete.css',
+                'https://cdnjs.cloudflare.com/ajax/libs/at.js/1.5.2/css/jquery.atwho.min.css',
+                )
+            }
         js = (
+            'https://cdnjs.cloudflare.com/ajax/libs/at.js/1.5.2/js/jquery.atwho.min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/Caret.js/0.3.1/jquery.caret.min.js',
             '/static/js/tinymce/tinymce.min.js',
             )
 
