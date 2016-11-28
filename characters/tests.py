27e6d62654a113795b8e9aa8246b19469f990e8f
Garrett <gn9012@gmail.com>
2016-11-27 21:50:30 -0600

    relocate form_data

--- characters/tests.py
+++ characters/tests.py
@@ -4,34 +4,35 @@ from . import forms
 
 
 class MonsterTestCase(TestCase):
+    form_data = {
+        'name': 'Sir Bearington',
+        'level': 20,
+        'alignment': 'LG',
+        'size': 'Large',
+        'languages': 'Common, Bear',
+        'strength': 20,
+        'dexterity': 20,
+        'constitution': 20,
+        'intelligence': 20,
+        'wisdom': 20,
+        'charisma': 20,
+        'armor_class': 18,
+        'hit_points': 100,
+        'speed': '30 ft.',
+        'saving_throws': 'None',
+        'skills': 'Cunning',
+        'creature_type': 'Bear',
+        'damage_vulnerabilities': 'Fire',
+        'damage_immunities': 'Slashing',
+        'condition_immunities': 'Blind',
+        'senses': 'Darkvision',
+        'challenge_rating': 10,
+        'traits': 'Looks like a bear, fights like a ninja',
+        'actions': 'Attacks you',
+        }
+
     def test_monster_form_success(self):
         """Successfully create a monster"""
-        form_data = {
-            'name': 'Sir Bearington',
-            'level': 20,
-            'alignment': 'LG',
-            'size': 'Large',
-            'languages': 'Common, Bear',
-            'strength': 20,
-            'dexterity': 20,
-            'constitution': 20,
-            'intelligence': 20,
-            'wisdom': 20,
-            'charisma': 20,
-            'armor_class': 18,
-            'hit_points': 100,
-            'speed': '30 ft.',
-            'saving_throws': 'None',
-            'skills': 'Cunning',
-            'creature_type': 'Bear',
-            'damage_vulnerabilities': 'Fire',
-            'damage_immunities': 'Slashing',
-            'condition_immunities': 'Blind',
-            'senses': 'Darkvision',
-            'challenge_rating': 10,
-            'traits': 'Looks like a bear, fights like a ninja',
-            'actions': 'Attacks you',
-            }
         form = forms.MonsterForm(data=form_data)
         self.assertTrue(form.is_valid())
 
