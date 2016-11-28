90f44dfa90f05d56a180f83333d78e7d2e0f3034
Garrett <gn9012@gmail.com>
2016-11-27 21:20:54 -0600

    comment out to start over

--- characters/tests.py
+++ characters/tests.py
@@ -1,61 +1,61 @@
-from django.test import TestCase
+# from django.test import TestCase
 
-from . import forms
-
-
-class MonsterTestCase(TestCase):
-    def test_monster_form_success(self):
-        """Successfully create a monster"""
-        form_data = {
-            'name': 'Sir Bearington',
-            'level': 20,
-            }
-        form = forms.MonsterForm(data=form_data)
-        self.assertTrue(form.is_valid())
-
-    def test_monster_form_bad_name_1(self):
-        """Fail to create a monster because of no name"""
-        form_data = {
-            'name': '',
-            'level': 20,
-            }
-        form = forms.MonsterForm(data=form_data)
-        self.assertFalse(form.is_valid())
-
-    def test_monster_form_bad_name_2(self):
-        """Fail to create a monster because of no level"""
-        form_data = {
-            'name': 'Sir Bearington',
-            'level': '',
-            }
-        form = forms.MonsterForm(data=form_data)
-        self.assertFalse(form.is_valid())
+# from . import forms
 
 
-class NPCTestCase(TestCase):
-    def test_npc_form_success(self):
-        """Successfully create an NPC"""
-        form_data = {
-            'name': 'Deckard Cain',
-            'level': 20,
-            }
-        form = forms.NPCForm(data=form_data)
-        self.assertTrue(form.is_valid())
+# class MonsterTestCase(TestCase):
+#     def test_monster_form_success(self):
+#         """Successfully create a monster"""
+#         form_data = {
+#             'name': 'Sir Bearington',
+#             'level': 20,
+#             }
+#         form = forms.MonsterForm(data=form_data)
+#         self.assertTrue(form.is_valid())
 
-    def test_npc_form_bad_name_1(self):
-        """Fail to create an NPC because of no name"""
-        form_data = {
-            'name': '',
-            'level': 20,
-            }
-        form = forms.NPCForm(data=form_data)
-        self.assertFalse(form.is_valid())
+#     def test_monster_form_bad_name_1(self):
+#         """Fail to create a monster because of no name"""
+#         form_data = {
+#             'name': '',
+#             'level': 20,
+#             }
+#         form = forms.MonsterForm(data=form_data)
+#         self.assertFalse(form.is_valid())
 
-    def test_npc_form_bad_name_2(self):
-        """Fail to create an NPC because of no level"""
-        form_data = {
-            'name': 'Sir Bearington',
-            'level': '',
-            }
-        form = forms.NPCForm(data=form_data)
-        self.assertFalse(form.is_valid())
+#     def test_monster_form_bad_name_2(self):
+#         """Fail to create a monster because of no level"""
+#         form_data = {
+#             'name': 'Sir Bearington',
+#             'level': '',
+#             }
+#         form = forms.MonsterForm(data=form_data)
+#         self.assertFalse(form.is_valid())
+
+
+# class NPCTestCase(TestCase):
+#     def test_npc_form_success(self):
+#         """Successfully create an NPC"""
+#         form_data = {
+#             'name': 'Deckard Cain',
+#             'level': 20,
+#             }
+#         form = forms.NPCForm(data=form_data)
+#         self.assertTrue(form.is_valid())
+
+#     def test_npc_form_bad_name_1(self):
+#         """Fail to create an NPC because of no name"""
+#         form_data = {
+#             'name': '',
+#             'level': 20,
+#             }
+#         form = forms.NPCForm(data=form_data)
+#         self.assertFalse(form.is_valid())
+
+#     def test_npc_form_bad_name_2(self):
+#         """Fail to create an NPC because of no level"""
+#         form_data = {
+#             'name': 'Sir Bearington',
+#             'level': '',
+#             }
+#         form = forms.NPCForm(data=form_data)
+#         self.assertFalse(form.is_valid())
