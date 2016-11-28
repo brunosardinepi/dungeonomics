1d4b9ecec2d867af63fe45285cbbea3813367d71
Garrett <gn9012@gmail.com>
2016-11-27 21:12:26 -0600

    test requirements

--- dungeonomics/tests.py
+++ dungeonomics/tests.py
@@ -3,14 +3,9 @@ from django.test import TestCase, RequestFactory
 
 from . import views
 
-class HomeTestCase(TestCase):
-    def setUp(self):
-        self.factory = RequestFactory()
-        self.user = User.objects.create_user(
-            username='test', email='test@email.com', password='password')
-
-    def test_home_view(self):
-        request = self.factory.get('home')
-        request.user = AnonymousUser()
-        response = views.HomeView.as_view()(request)
-        self.assertEqual(response.status_code, 200)
+# signup
+# login
+# logout
+# view campaign when not logged in
+# view campaign while logged in
+# crud campaign/monster/npc
