ab0db92da250d30bb24fe14e9f32483d4e33b7d7
Garrett <gn9012@gmail.com>
2016-11-28 16:10:26 -0600

    create ChapterAdmin, SectionAdmin

--- campaign/admin.py
+++ campaign/admin.py
@@ -8,4 +8,16 @@ class CampaignAdmin(admin.ModelAdmin):
   # ordering = ('-created_at',)
   list_filter = ('user',)
 
+class ChapterAdmin(admin.ModelAdmin):
+  list_display = ('title', 'user', 'created_at', 'campaign',)
+  # ordering = ('-created_at',)
+  list_filter = ('user', 'campaign',)
+
+class SectionAdmin(admin.ModelAdmin):
+  list_display = ('title', 'user', 'created_at', 'campaign', 'chapter',)
+  # ordering = ('-created_at',)
+  list_filter = ('user', 'campaign', 'chapter',)
+
 admin.site.register(models.Campaign, CampaignAdmin)
+admin.site.register(models.Chapter, ChapterAdmin)
+admin.site.register(models.Section, SectionAdmin)
