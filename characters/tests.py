37e7787d8fe11b9507cc1ac7aacb39eeb76710a0
Garrett <gn9012@gmail.com>
2016-11-27 21:55:12 -0600

    self

--- characters/tests.py
+++ characters/tests.py
@@ -38,7 +38,7 @@ class MonsterTestCase(TestCase):
 
     def test_monster_form_bad_name(self):
         """Fail to create a monster because of no name"""
-        form_data['name'] = ''
+        self.form_data['name'] = ''
         form = forms.MonsterForm(data=self.form_data)
         self.assertFalse(form.is_valid())
 
