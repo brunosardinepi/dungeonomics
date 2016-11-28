75c9e869c7e57e7de25df47f87d0f1d8229d4be7
Garrett <gn9012@gmail.com>
2016-11-27 22:00:15 -0600

    form_data_test

--- characters/tests.py
+++ characters/tests.py
@@ -33,70 +33,21 @@ class MonsterTestCase(TestCase):
 
     def test_monster_form_success(self):
         """Successfully create a monster"""
-        form_data = {
-            'name': 'Sir Bearington',
-            'level': 20,
-            'alignment': 'LG',
-            'size': 'Large',
-            'languages': 'Common, Bear',
-            'strength': 20,
-            'dexterity': 20,
-            'constitution': 20,
-            'intelligence': 20,
-            'wisdom': 20,
-            'charisma': 20,
-            'armor_class': 18,
-            'hit_points': 100,
-            'speed': '30 ft.',
-            'saving_throws': 'None',
-            'skills': 'Cunning',
-            'creature_type': 'Bear',
-            'damage_vulnerabilities': 'Fire',
-            'damage_immunities': 'Slashing',
-            'condition_immunities': 'Blind',
-            'senses': 'Darkvision',
-            'challenge_rating': 10,
-            'traits': 'Looks like a bear, fights like a ninja',
-            'actions': 'Attacks you',
-            }
-        form = forms.MonsterForm(data=form_data)
+        form = forms.MonsterForm(data=self.form_data)
         self.assertTrue(form.is_valid())
 
     def test_monster_form_bad_name(self):
         """Fail to create a monster because of no name"""
-        self.form_data['name'] = ''
-        form = forms.MonsterForm(data=self.form_data)
+        form_data_test = dict(form_data)
+        form_data_test['name'] = ''
+        form = forms.MonsterForm(data=form_data_test)
         self.assertFalse(form.is_valid())
 
     def test_monster_form_bad_level(self):
         """Fail to create a monster because of no level"""
-        form_data = {
-            'name': 'Sir Bearington',
-            'level': '',
-            'alignment': 'LG',
-            'size': 'Large',
-            'languages': 'Common, Bear',
-            'strength': 20,
-            'dexterity': 20,
-            'constitution': 20,
-            'intelligence': 20,
-            'wisdom': 20,
-            'charisma': 20,
-            'armor_class': 18,
-            'hit_points': 100,
-            'speed': '30 ft.',
-            'saving_throws': 'None',
-            'skills': 'Cunning',
-            'creature_type': 'Bear',
-            'damage_vulnerabilities': 'Fire',
-            'damage_immunities': 'Slashing',
-            'condition_immunities': 'Blind',
-            'senses': 'Darkvision',
-            'challenge_rating': 10,
-            'traits': 'Looks like a bear, fights like a ninja',
-            'actions': 'Attacks you',
-        }
-        form = forms.MonsterForm(data=form_data)
+        form_data_test = dict(form_data)
+        form_data_test['level'] = ''
+        form = forms.MonsterForm(data=form_data_test)
         self.assertFalse(form.is_valid())
 
 
