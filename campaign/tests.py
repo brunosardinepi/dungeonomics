8833fd470da8ea102f6e5f9f2abded5cb0983faa
Garrett <gn9012@gmail.com>
2016-11-27 21:23:32 -0600

    campaign creation

--- campaign/tests.py
+++ campaign/tests.py
@@ -1,19 +1,19 @@
-# from django.contrib.auth.models import User
-# from django.test import TestCase
-# from django.utils import timezone
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.utils import timezone
 
-# from . import models
+from . import models
 
 
-# class CampaignModelTests(TestCase):
-#     def setUp(self):
-#         self.user = User.objects.create(username="testuser", password="testpassword")
+class CampaignModelTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username="testuser", password="testpassword")
 
-#     def test_campaign_creation(self):
-#         campaign = models.Campaign.objects.create(
-#             user=self.user,
-#             title="Campaign Title",
-#             description="This is the campaign description."
-#         )
-#         now = timezone.now()
-#         self.assertLess(campaign.created_at, now)
+    def test_campaign_creation(self):
+        campaign = models.Campaign.objects.create(
+            user=self.user,
+            title="Campaign Title",
+            description="This is the campaign description."
+        )
+        now = timezone.now()
+        self.assertLess(campaign.created_at, now)
