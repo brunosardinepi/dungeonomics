b094d5c032849fa23be483bca283e64fea8bbed7
Garrett <gn9012@gmail.com>
2016-11-27 21:19:26 -0600

    hometest

--- dungeonomics/tests.py
+++ dungeonomics/tests.py
@@ -1,10 +1,18 @@
-from django.contrib.auth.models import AnonymousUser, User
-from django.test import TestCase, RequestFactory
+import unittest
+from django.test import Client
 
-from . import views
+class HomeTest(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_details(self):
+        response = self.client.get('home')
+        self.assertEqual(response.status_code, 200)
 
 # signup
 # login
+    # dungeonomics_test
+    # 2YmP3sggAQ9p
 # logout
 # view campaign when not logged in
 # view campaign while logged in
