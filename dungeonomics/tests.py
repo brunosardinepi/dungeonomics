d52797ea9b3805923c981cd4633189f0a7d6e6b3
Garrett <gn9012@gmail.com>
2016-11-27 21:22:49 -0600

    start over

--- dungeonomics/tests.py
+++ dungeonomics/tests.py
@@ -1,14 +1,3 @@
-import unittest
-from django.test import Client
-
-class HomeTest(unittest.TestCase):
-    def setUp(self):
-        self.client = Client()
-
-    def test_details(self):
-        response = self.client.get('home')
-        self.assertEqual(response.status_code, 200)
-
 # signup
 # login
     # dungeonomics_test
