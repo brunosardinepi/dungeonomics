dc6e9372533c4f702b51a7abd3a518f2617c873f
Garrett <gn9012@gmail.com>
2016-11-29 22:08:26 -0600

    typo

--- wiki/views.py
+++ wiki/views.py
@@ -29,7 +29,7 @@ def wiki_home(request, section_pk=None, subsection_pk=None):
                 this_subsection = None
 
         if this_subsection:
-            return render(request, 'wiki/home.html', {'this_section': this_section, 'this_subsection': this_subsection, 'sections': sections, subsections': subsections})
+            return render(request, 'wiki/home.html', {'this_section': this_section, 'this_subsection': this_subsection, 'sections': sections, 'subsections': subsections})
         else:
             return render(request, 'campaign/campaign_detail.html', {'this_section': this_section, 'sections': sections, 'subsections': subsections})
     else:
