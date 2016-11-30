1adfd01222fff7abbb98d25b9b870f08cd4172be
Garrett <gn9012@gmail.com>
2016-11-29 18:12:25 -0600

    remove password

--- dungeonomics/settings.py
+++ dungeonomics/settings.py
@@ -148,7 +148,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'dungeonomics'
-EMAIL_HOST_PASSWORD = 'B6uv5jqIyV'
+
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'Dungeonomics <no-reply@dungeonomics.com>'
 
