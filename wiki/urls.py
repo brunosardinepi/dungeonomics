f773d7b05ddae248ac70e09a475df06d557e8db5
Garrett <gn9012@gmail.com>
2016-11-29 21:16:08 -0600

    home urls

--- wiki/urls.py
+++ wiki/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # url(r'^$', views.wiki_home, name='home'),
+    url(r'^$', views.wiki_home, name='home'),
     url(r'^(?P<section_pk>\d+)/$', views.wiki_home, name='home'),
     url(r'^(?P<section_pk>\d+)/chapter/(?P<subsection_pk>\d+)/$', views.wiki_home, name='home'),
 ]
