9f635bc00c6d8a46bddd6111a36c79ebc8103b86
Garrett <gn9012@gmail.com>
2016-11-29 22:07:38 -0600

    fixing view

--- wiki/views.py
+++ wiki/views.py
@@ -5,27 +5,37 @@ from . import models
 
 
 def wiki_home(request, section_pk=None, subsection_pk=None):
+    sections = sorted(models.Section.objects.all(),
+        key=lambda section: section.title)
+
+    subsections = []
+    for section in sections:
+        subsections.append(sorted(
+            models.Subsection.objects.filter(section=section),
+            key=lambda subsection: subsection.order
+            ))
+    subsections = [item for sublist in subsections for item in sublist]
+
     if section_pk:
         this_section = get_object_or_404(models.Section, pk=section_pk)
-        subsections = sorted(models.Subsection.objects.filter(section=this_section),
+        this_subsections = sorted(models.Subsection.objects.filter(section=this_section),
         key=lambda subsection: subsection.order)
-
         if subsection_pk:
             this_subsection = get_object_or_404(models.Subsection, pk=subsection_pk)
         else:
-            if len(subsections) > 0:
-                this_subsection = subsections[0]
+            if len(this_subsections) > 0:
+                this_subsection = this_subsections[0]
             else:
                 this_subsection = None
 
         if this_subsection:
-            return render(request, 'wiki/home.html', {'this_section': this_section, 'this_subsection': this_subsection, 'subsections': subsections})
+            return render(request, 'wiki/home.html', {'this_section': this_section, 'this_subsection': this_subsection, 'sections': sections, subsections': subsections})
         else:
-            return render(request, 'campaign/campaign_detail.html', {'this_section': this_section})
+            return render(request, 'campaign/campaign_detail.html', {'this_section': this_section, 'sections': sections, 'subsections': subsections})
     else:
         this_section = None
         sections = sorted(models.Section.objects.all(),
             key=lambda section: section.title)
         if len(sections) > 0:
             this_section = sections[0]
-        return render(request, 'wiki/home.html', {'this_section': this_section, 'sections': sections})
+        return render(request, 'wiki/home.html', {'this_section': this_section, 'sections': sections, 'subsections': subsections})
