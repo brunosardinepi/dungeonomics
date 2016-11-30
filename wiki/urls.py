52186a426afbcb75f3dbb4c23c195ca3ccf2aa5e
Garrett <gn9012@gmail.com>
2016-11-30 17:24:08 -0600

    fix urls

--- wiki/urls.py
+++ wiki/urls.py
@@ -7,13 +7,12 @@ urlpatterns = [
     url(r'^(?P<section_pk>\d+)/subsection/(?P<subsection_pk>\d+)/$', views.wiki_home, name='home'),
     url(r'^$', views.wiki_home, name='home'),
 
-    url(r'^create/$', views.SectionCreate.as_view(), name='section_create'),
-    url(r'^(?P<section_pk>\d+)/section/create/$', views.section_create, name='section_create'),
+    url(r'^create/$', views.section_create, name='section_create'),
     url(r'^(?P<section_pk>\d+)/section/(?P<subsection_pk>\d+)/subsection/create/$', views.subsection_create, name='subsection_create'),
 
     url(r'^(?P<section_pk>\d+)/edit/$', views.section_update, name='section_update'),
     url(r'^(?P<section_pk>\d+)/chapter/(?P<subsection_pk>\d+)/edit/$', views.subsection_update, name='subsection_update'),
 
-    url(r'^(?P<section_pk>\d+)/delete/$', views.SectionDelete.as_view(), name='section_delete'),
-    url(r'^(?P<section_pk>\d+)/chapter/(?P<subsection_pk>\d+)/delete/$', views.SubsectionDelete.as_view(), name='subsection_delete'),
+    url(r'^(?P<section_pk>\d+)/delete/$', views.section_delete, name='section_delete'),
+    url(r'^(?P<section_pk>\d+)/chapter/(?P<subsection_pk>\d+)/delete/$', views.subsection_delete, name='subsection_delete'),
 ]
