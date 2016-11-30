43baad320c151ad13623f9bed7350cc17cf3e9e4
Garrett <gn9012@gmail.com>
2016-11-29 20:03:25 -0600

    wiki models setup

--- wiki/models.py
+++ wiki/models.py
@@ -1,3 +1,42 @@
+from django.core.urlresolvers import reverse
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class SectionTemplate(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class Section(SectionTemplate):
+    content = models.TextField(blank=True)
+    order = models.IntegerField(default=1)
+
+    class Meta:
+        ordering = ['order', 'title', ]
+
+    def get_absolute_url(self):
+        return reverse('wiki:home', kwargs={
+            'section_pk': self.pk
+            })
+
+
+class Subsection(SectionTemplate):
+    content = models.TextField(blank=True)
+    order = models.IntegerField(verbose_name= _('Section number'),default=1)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['order', 'title',]
+
+    def get_absolute_url(self):
+        return reverse('wiki:home', kwargs={
+            'section_pk': self.section_id,
+            'subsection_pk': self.pk
+            })
--- wiki/views.py
+++ wiki/views.py
@@ -13,5 +13,32 @@ from django.shortcuts import get_object_or_404, render
 #             this_section = sections[0]
 #         return render(request, 'wiki/wiki_home.html', {'this_section': this_section})
 
-def wiki_home(request):
-    return HttpResponse("wiki")
+# def wiki_home(request):
+#     return HttpResponse("wiki")
+
+
+def wiki_home(request, section_pk=None, subsection_pk=None):
+    if section_pk:
+        this_section = get_object_or_404(models.Section, pk=section_pk)
+        subsections = sorted(models.Subsection.objects.filter(section=this_section),
+        key=lambda subsection: subsection.order)
+
+        if subsection_pk:
+            this_subsection = get_object_or_404(models.Subsection, pk=subsection_pk)
+        else:
+            if len(subsections) > 0:
+                this_subsection = subsections[0]
+            else:
+                this_subsection = None
+
+        if this_subsection:
+            return render(request, 'wiki/home.html', {'this_section': this_section, 'this_subsection': this_subsection, 'subsections': subsections})
+        else:
+            return render(request, 'campaign/campaign_detail.html', {'this_subsection': this_section})
+    else:
+        this_section = None
+        sections = sorted(models.Section.objects.all(),
+            key=lambda section: section.title)
+        if len(sections) > 0:
+            this_section = sections[0]
+        return render(request, 'wiki/home.html', {'this_section': this_section})
