57c8a6fddafdce2b25d19e08b3c7927450fdc444
Garrett <gn9012@gmail.com>
2016-11-29 21:19:57 -0600

    initial admin

--- wiki/admin.py
+++ wiki/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class SectionAdmin(admin.ModelAdmin):
+  list_display = ('title', 'order',)
+
+class SubsectionAdmin(admin.ModelAdmin):
+  list_display = ('title', 'order', 'section',)
+  list_filter = ('section',)
+
+admin.site.register(models.Section, SectionAdmin)
+admin.site.register(models.Subsection, SubsectionAdmin)
