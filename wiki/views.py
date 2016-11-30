6f888a2d0309d4edeb6491ee40f7f4650a322d10
Garrett <gn9012@gmail.com>
2016-11-29 19:52:03 -0600

    wiki home page

--- wiki/views.py
+++ wiki/views.py
@@ -1,17 +1,13 @@
-from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
 
-# def wiki_home(request, section_pk=None):
-#     if section_pk:
-#         this_section = get_object_or_404(models.Section, pk=section_pk)
-#         return render(request, 'wiki/wiki_home.html', {'this_section': this_section})
-#     else:
-#         sections = sorted(models.Section.objects.all(),
-#             key=lambda section: section.title)
-#         if len(sections) > 0:
-#             this_section = sections[0]
-#         return render(request, 'wiki/wiki_home.html', {'this_section': this_section})
-
-def wiki_home(request):
-    return HttpResponse("wiki")
+def wiki_home(request, section_pk=None):
+    if section_pk:
+        this_section = get_object_or_404(models.Section, pk=section_pk)
+        return render(request, 'wiki/wiki_home.html', {'this_section': this_section})
+    else:
+        sections = sorted(models.Section.objects.all(),
+            key=lambda section: section.title)
+        if len(sections) > 0:
+            this_section = sections[0]
+        return render(request, 'wiki/wiki_home.html', {'this_section': this_section})
