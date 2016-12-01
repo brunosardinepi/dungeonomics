401487b04ccd14022760121b3837a8ad70b939c7
Garrett <gn9012@gmail.com>
2016-11-30 18:57:23 -0600

    import reverse_lazy

--- wiki/views.py
+++ wiki/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic.edit import DeleteView
 
