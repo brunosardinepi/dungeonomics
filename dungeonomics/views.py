c5f53fd4e9be73b6b2eb0c4d4bba56ee4438aeb8
Garrett <gn9012@gmail.com>
2016-12-03 12:45:16 -0600

    import HttpResponseRedirect

--- dungeonomics/views.py
+++ dungeonomics/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import TemplateView
 
