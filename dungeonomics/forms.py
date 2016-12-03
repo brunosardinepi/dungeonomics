83223ada0811e9eb27d166cbe229f78a92382acf
Garrett <gn9012@gmail.com>
2016-12-03 11:17:42 -0600

    initial delete_account setup

--- dungeonomics/forms.py
+++ dungeonomics/forms.py
@@ -1,20 +1,22 @@
-from allauth.account.forms import LoginForm
+from django import forms
 
-# class MyLoginForm(LoginForm):
-#     def __init__(self, *args, **kwargs):
-#         super(MyLoginForm, self).__init__(*args, **kwargs)
-#         self.fields['password'].widget = forms.PasswordInput()
+from . import models
 
-#         if 'remember' in self.fields.keys():
-#             del self.fields['remember']
+class DeleteUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['is_active']
 
-#         helper = FormHelper()
-#         helper.form_show_labels = False
-#         helper.layout = Layout(
-#             Field('login', placeholder = 'Email address'),
-#             Field('password', placeholder = 'Password'),
-#             FormActions(
-#                 Submit('submit', 'Log me in to Cornell Forum', css_class = 'btn-primary')
-#             ),
-#         )
-#         self.helper = helper
+    def __init__(self, *args, **kwargs):
+        super(DeleteUserForm, self).__init__(*args, **kwargs)
+        self.fields['is_active'].help_text = "Check this box if you are sure you want to delete this account."
+
+    def clean_is_active(self):
+        # Reverses true/false for your form prior to validation
+        #
+        # You can also raise a ValidationError here if you receive 
+        # a value you don't want, to prevent the form's is_valid 
+        # method from return true if, say, the user hasn't chosen 
+        # to deactivate their account
+        is_active = not(self.cleaned_data["is_active"])
+        return is_active
--- dungeonomics/views.py
+++ dungeonomics/views.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
 from allauth.account import views
+
+from . import forms
 
 
 class HomeView(TemplateView):
@@ -34,3 +37,20 @@ class EmailVerificationSentView(views.EmailVerificationSentView):
 
 class PasswordResetView(views.PasswordResetView):
     template_name = 'password_reset.html'
+
+
+@login_required
+def delete_account(request):  
+    pk = request.user.id
+    user = User.objects.get(pk=pk)
+    user_form = forms.DeleteUserForm(instance=user)
+    if request.user.is_authenticated() and request.user.id == user.id:
+        if request.method == "POST":
+            user_form = DeleteUserForm(request.POST, instance=user)
+            if user_form.is_valid():
+                deactivate_user = user_form.save(commit=False)
+                user.is_active = False
+                deactivate_user.save()
+        return render(request, "delete_account.html", {'user_form': user_form})
+    else:
+        raise PermissionDenied
