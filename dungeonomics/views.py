06a79a2223b2e0435c55a53ff9e9727cfeef3123
Garrett <gn9012@gmail.com>
2016-12-03 12:41:54 -0600

    import messages

--- dungeonomics/views.py
+++ dungeonomics/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render
