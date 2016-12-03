18ac649090daa7d7ca0c541ab69465f6ace23ff1
Garrett <gn9012@gmail.com>
2016-12-03 12:30:00 -0600

    import

--- dungeonomics/views.py
+++ dungeonomics/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.views.generic import TemplateView
 
 from allauth.account import views
