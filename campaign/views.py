c6b59b5c0bbd2f9758938484f6960f295ef900a1
Garrett <gn9012@gmail.com>
2016-12-06 19:33:18 -0600

    import reverse

--- campaign/views.py
+++ campaign/views.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse_lazy
