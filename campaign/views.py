8a9aa34741ffe15c636fc8e5a150a4382eb243ff
Garrett <gn9012@gmail.com>
2016-12-16 16:06:10 -0600

    fix #58

--- campaign/views.py
+++ campaign/views.py
@@ -330,7 +330,7 @@ def campaign_import(request):
             campaign.user = request.user
             campaign.save()
             for chapter in user_import["chapters"]:
-                chapter = json.loads(chapter)
+                chapter = dict(chapter)
                 campaign_import_chapter_create(request.user, campaign, chapter)
             return HttpResponseRedirect(campaign.get_absolute_url())
     return render(request, 'campaign/campaign_import.html', {'form': form, 'user_import': user_import})
