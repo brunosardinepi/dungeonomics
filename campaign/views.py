5bec8317c4cbadbd64a92a1fa70a605aa789820b
Garrett <gn9012@gmail.com>
2016-12-16 14:55:18 -0600

    fix #58 -- troubleshooting output

--- campaign/views.py
+++ campaign/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect, Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
