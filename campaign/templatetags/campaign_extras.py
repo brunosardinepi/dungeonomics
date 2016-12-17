6d2874f0cb7b2f062331391bebc7b3148cc59795
Garrett <gn9012@gmail.com>
2016-12-17 10:09:31 -0600

    fix #58

--- campaign/templatetags/campaign_extras.py
+++ campaign/templatetags/campaign_extras.py
@@ -11,14 +11,12 @@ register = template.Library()
 
 def jsonify(value): 
     '''Convert Django object to JSON object'''
-    # return mark_safe(json.dumps(value))
     return serializers.serialize("json", value)
 
 @register.simple_tag
 def sections_in_chapter(chapter_pk):
     '''Returns dictionary of sections to display in export'''
     chapter = models.Chapter.objects.get(pk=chapter_pk)
-    # sections = models.Section.objects.filter(chapter=chapter).order_by('order')
     sections = sorted(models.Section.objects.filter(chapter=chapter),
             key=lambda section: section.order
             )
