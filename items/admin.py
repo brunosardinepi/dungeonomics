037d7e5a621040b02e499a9b545e20b3373da174
Garrett <gn9012@gmail.com>
2016-12-22 20:32:02 -0600

    fix #23

--- items/admin.py
+++ items/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class ItemAdmin(admin.ModelAdmin):
+  list_display = ('name', 'user',)
+  list_filter = ('user',)
+
+admin.site.register(models.Item, ItemAdmin)
