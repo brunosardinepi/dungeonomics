695178f13ac109294764bef6cf98720263eafeac
Garrett <gn9012@gmail.com>
2017-01-07 10:34:32 -0600

    fix #41

--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class LocationTemplate(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class World(LocationTemplate):
+    content = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ['title',]
+
+    def get_absolute_url(self):
+        return reverse('locations:location_detail', kwargs={
+            'world_pk': self.pk
+            })
+
+
+class Location(LocationTemplate):
+    content = models.TextField(blank=True)
+    world = models.ForeignKey(World, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['title',]
+
+    def get_absolute_url(self):
+        return reverse('locations:location_detail', kwargs={
+            'world_pk': self.world_id,
+            'location_pk': self.pk
+            })
