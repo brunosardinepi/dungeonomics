32dd3fc545f1f4aa4c7ceb597e7e9583b6368806
Garrett <gn9012@gmail.com>
2017-02-14 22:45:25 +0000

    updated admin to include locations

--- locations/admin.py
+++ locations/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class WorldAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('name',)
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'world',)
+    ordering = ('name', 'world',)
+    list_filter = ('name', 'world',)
+
+admin.site.register(models.World, WorldAdmin)
+admin.site.register(models.Location, LocationAdmin)
