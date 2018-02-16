b30845bf5b184fdbf636a38f6e366035725d2aa0
brunosardinepi <gn9012@gmail.com>
2018-02-16 21:56:38 +0000

    #90 -- updated tests for items

--- items/tests.py
+++ items/tests.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.test import Client, RequestFactory, TestCase
+from django.test import Client, TestCase
 
 import unittest
 
@@ -10,27 +10,30 @@ from . import views
 
 class ItemTest(TestCase):
     item_form_data = {
-        'name': 'test item',
+        'name': 'test item name',
     }
 
     def setUp(self):
-        self.factory = RequestFactory()
         self.client = Client()
 
-        # create a test user
-        self.user = User.objects.create_user(username='testuser', email='test@test.test', password='testpassword')
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='test@test.test',
+            password='testpassword',
+        )
 
-        # create a test user
-        self.user2 = User.objects.create_user(username='testuser2', email='test2@test.test', password='testpassword')
+        self.user2 = User.objects.create_user(
+            username='testuser2',
+            email='test2@test.test',
+            password='testpassword',
+        )
 
-        # create a test item for testuser
         self.item = models.Item.objects.create(
             user=self.user,
             name="test item",
             description="this is test item",
         )
 
-        # create a test item for testuser2
         self.item2 = models.Item.objects.create(
             user=self.user2,
             name="test 2 item",
@@ -38,143 +41,64 @@ class ItemTest(TestCase):
         )
 
     def test_item_exists(self):
-        """
-        Test items exist
-        """
-
-        # get queryset that contains all items
         items = models.Item.objects.all()
-
-        # make sure the test items exist in the queryset
         self.assertIn(self.item, items)
         self.assertIn(self.item2, items)
 
     def test_item_page(self):
-        """
-        Item page contains the correct information
-        """
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get('/items/{}/'.format(self.item.pk))
 
-        # create an instance of a GET request
-        request = self.factory.get('home')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.item.name)
+        self.assertContains(response, self.item.description)
 
-        # simulate a logged-in user
-        request.user = self.user
-
-        # test the view
-        response = views.item_detail(request, self.item.pk)
-
-        # make sure the item information is on the page
-        self.assertContains(response, self.item.name, status_code=200)
-        self.assertContains(response, self.item.description, status_code=200)
-
-    @unittest.expectedFailure
     def test_item_page_bad_user(self):
-        """
-        Item page is inaccessible by the wrong user
-        """
-
-        # create an instance of a GET request
-        request = self.factory.get('home')
-
-        # simulate a logged-in user
-        request.user = self.user
-
-        # test the view
-        response = views.item_detail(request, self.item2.pk)
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get('/items/{}/'.format(self.item2.pk))
+        self.assertEqual(response.status_code, 404)
 
     def test_item_create_page(self):
-        """
-        Create item page loads
-        """
-
-        # create an instance of a GET request
-        request = self.factory.get('home')
-
-        # simulate a logged-in user
-        request.user = self.user
-
-        # test the view
-        response = views.item_create(request)
-
-        # check that the response is 200 OK
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get('/items/create/')
         self.assertEqual(response.status_code, 200)
 
     def test_item_create(self):
-        """
-        Create item
-        """
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.post('/items/create/', self.item_form_data)
+        item = models.Item.objects.get(name='test item name')
+        self.assertRedirects(response, '/items/{}/'.format(item.pk), 302, 200)
 
-        form = forms.ItemForm(data=self.item_form_data)
-        new_item = form.save(commit=False)
-        new_item.user = self.user
-        new_item.save()
-
-        new_item = models.Item.objects.get(pk=new_item.pk)
         items = models.Item.objects.all()
-
-        self.assertTrue(form.is_valid())
-        self.assertIn(new_item, items)
+        self.assertIn(item, items)
 
     def test_item_edit_page(self):
-        """
-        Edit item page contains the correct information
-        """
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get('/items/{}/edit/'.format(self.item.pk))
 
-        # create an instance of a GET request
-        request = self.factory.get('home')
-
-        # simulate a logged-in user
-        request.user = self.user
-
-        # test the view
-        response = views.item_update(request, self.item.pk)
-
-        # make sure the item information is on the page
-        self.assertContains(response, self.item.name, status_code=200)
-        self.assertContains(response, self.item.description, status_code=200)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.item.name)
 
     def test_item_edit(self):
-        """
-        Edit item
-        """
+        self.client.login(username='testuser', password='testpassword')
+        self.item_form_data['name'] = "test item name EDIT"
+        response = self.client.post('/items/{}/edit/'.format(self.item.pk), self.item_form_data)
+        self.assertRedirects(response, '/items/{}/'.format(self.item.pk), 302, 200)
 
-        self.item_form_data['name'] = "test EDIT item"
-        form = forms.ItemForm(data=self.item_form_data)
-        new_item = form.save(commit=False)
-        new_item.user = self.user
-        new_item.save()
-
-        verify_item = models.Item.objects.get(pk=new_item.pk)
-        items = models.Item.objects.all()
-
-        self.assertTrue(form.is_valid())
-        self.assertEqual(new_item.name, verify_item.name)
+        response = self.client.get('/items/{}/'.format(self.item.pk))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'test item name EDIT')
 
     def test_item_delete_page(self):
-        """
-        Delete item page contains the correct information
-        """
-
-        # create an instance of a GET request
-        request = self.factory.get('home')
-
-        # simulate a logged-in user
-        request.user = self.user
-
-        # test the view
-        response = views.item_delete(request, self.item.pk)
-
-        # make sure the item information is on the page
-        self.assertContains(response, self.item.name, status_code=200)
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get('/items/{}/delete/'.format(self.item.pk))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.item.name)
 
     def test_item_delete(self):
-        """
-        Delete item
-        """
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.post('/items/{}/delete/'.format(self.item.pk), {})
+        self.assertRedirects(response, '/items/', 302, 200)
 
-        form = forms.DeleteItemForm(data=self.item_form_data)
-        new_item = form.save(commit=False)
-        new_item.user = self.user
-        new_item.save()
-
-        self.assertTrue(form.is_valid())
+        items = models.Item.objects.all()
+        self.assertEqual(items.count(), 1)
