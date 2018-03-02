16a5aa9a51ab2efb01fa029eda7945a9f6ceffd0
brunosardinepi <gn9012@gmail.com>
2018-03-02 17:24:15 +0000

    #89 -- login design redo

--- dungeonomics/settings.py
+++ dungeonomics/settings.py
@@ -27,12 +27,12 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.facebook',
     'lib',
-    'bootstrap3',
     'campaign',
     'characters',
     'wiki',
     'items',
     'locations',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
