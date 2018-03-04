eb032bbf482ceb0bf5b19480eb5f991f4e857fc4
brunosardinepi <gn9012@gmail.com>
2018-03-04 04:19:10 +0000

    #89 -- player form design

--- characters/views.py
+++ characters/views.py
@@ -313,7 +313,7 @@ def player_update(request, player_pk):
             form = forms.PlayerForm(instance=player, data=request.POST)
             if form.is_valid():
                 form.save()
-                messages.add_message(request, messages.SUCCESS, "Updated player: {}".format(form.cleaned_data['player_name']))
+                messages.add_message(request, messages.SUCCESS, "Updated player")
                 return HttpResponseRedirect(player.get_absolute_url())
     else:
         raise Http404
