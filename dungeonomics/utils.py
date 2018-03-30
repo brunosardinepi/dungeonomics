d146f8ba4e41f2bf5c900a530b148a4352208af3
brunosardinepi <gn9012@gmail.com>
2018-03-30 19:53:24 +0000

    #27 -- fixed edit with no image changes

--- dungeonomics/utils.py
+++ dungeonomics/utils.py
@@ -5,11 +5,12 @@ def image_is_valid(request, form):
     if form.is_valid():
         image_raw = form.cleaned_data.get('image', False)
         if image_raw:
-            image_type = image_raw.content_type.split('/')[0]
-            if image_type in settings.UPLOAD_TYPES:
-                if image_raw._size <= settings.MAX_IMAGE_UPLOAD_SIZE:
-                    return True
+            if type(image_raw) == 'django.core.files.uploadedfile.InMemoryUploadedFile':
+                image_type = image_raw.content_type.split('/')[0]
+                if image_type in settings.UPLOAD_TYPES:
+                    if image_raw._size <= settings.MAX_IMAGE_UPLOAD_SIZE:
+                        return True
+                    else:
+                        return "bad size"
                 else:
-                    return "bad size"
-            else:
-                return "bad type"
+                    return "bad type"
