366764d49e8dea0f4d22630c85a0787007dc79d1
brunosardinepi <gn9012@gmail.com>
2018-11-12 19:50:00 +0000

    #50 -- updated @ tagging

--- dungeonomics/utils.py
+++ dungeonomics/utils.py
@@ -2,20 +2,11 @@ from django.conf import settings
 from django.db.models import Avg
 
 from tavern.models import Review
-from characters.models import Monster, NPC, Player
+from characters.models import GeneralCharacter
 from items.models import Item
 from locations.models import Location, World
 from tables.models import Table
 
-
-suggested_attributes = [
-    'a',
-    'b',
-    'c',
-    'ccc',
-    'bad',
-    'ball',
-]
 
 def image_is_valid(request, form):
     if form.is_valid():
@@ -34,22 +25,14 @@ def image_is_valid(request, form):
                     return "bad type"
 
 def at_tagging(request):
-    monsters_raw = Monster.objects.filter(user=request.user).order_by('name')
-    monsters = {}
-    for monster in monsters_raw:
-        monsters[monster.pk] = monster.name
-    npcs_raw = NPC.objects.filter(user=request.user).order_by('name')
-    npcs = {}
-    for npc in npcs_raw:
-        npcs[npc.pk] = npc.name
+    characters_raw = GeneralCharacter.objects.filter(user=request.user).order_by('name')
+    characters = {}
+    for character in characters_raw:
+        characters[character.pk] = character.name
     items_raw = Item.objects.filter(user=request.user).order_by('name')
     items = {}
     for item in items_raw:
         items[item.pk] = item.name
-    players_raw = Player.objects.filter(user=request.user).order_by('character_name')
-    players = {}
-    for player in players_raw:
-        players[player.pk] = player.character_name
     worlds_raw = World.objects.filter(user=request.user).order_by('name')
     worlds = {}
     for world in worlds_raw:
@@ -64,10 +47,8 @@ def at_tagging(request):
         tables[table.pk] = table.name
 
     return {
-        'monsters': monsters,
-        'npcs': npcs,
+        'characters': characters,
         'items': items,
-        'players': players,
         'worlds': worlds,
         'locations': locations,
         'tables': tables,
