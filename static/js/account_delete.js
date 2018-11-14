07d93f369f37f41ed00c522a3d0a1beafc77d816
brunosardinepi <gn9012@gmail.com>
2018-11-14 00:06:11 +0000

    fixed gitignore files

--- static/js/account_delete.js
+++
@@ -1,3 +0,0 @@
-$(document).on('click', '#account-delete', function() {
-    return confirm("Are you sure you want to delete your account? It will be gone forever!")
-});
--- static/js/character_delete.js
+++
@@ -1,3 +0,0 @@
-$(document).on('click', '#character-delete', function() {
-    return confirm("Are you sure you want to delete this character? It will be gone forever!")
-});
