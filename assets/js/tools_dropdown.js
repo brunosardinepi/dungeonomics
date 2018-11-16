02ff9115f4b0fe78009ea05ffeb53c7981983bc0
brunosardinepi <gn9012@gmail.com>
2018-11-16 22:31:12 +0000

    #129 -- animated settings cog

--- assets/js/tools_dropdown.js
+++ assets/js/tools_dropdown.js
@@ -1,9 +1,17 @@
 $(document).on('click', '[id$="-tools-dropdown"]', function(event) {
     event.preventDefault();
 
+    if (!$(this).hasClass('rotate-cw-180') && !$(this).hasClass('rotate-ccw-180')) {
+        // fresh start, rotate-cw-180
+        $(this).toggleClass('rotate-cw-180');
+    } else {
+        // used before, so rotate-ccw-180
+        $(this).toggleClass('rotate-cw-180 rotate-ccw-180');
+    };
+
     var dropdownName = $(this).attr('id').split("-")[0];
     var toolsDiv = $("#" + dropdownName + "-tools").get();
-    var animationSpeed = 400;
+    var animationSpeed = 500;
 
     $(toolsDiv).slideToggle(animationSpeed);
 });
