6c3bc3a9be67c5a0cd003cb0928c6a5187c815e9
Garrett <gn9012@gmail.com>
2018-11-16 18:42:51 +0000

    fixed gitignore files

--- static/js/item_delete.js
+++
@@ -1,3 +0,0 @@
-$(document).on('click', '#item-delete', function() {
-    return confirm("Are you sure you want to delete this item/spell? It will be gone forever!")
-});
--- static/js/location_delete.js
+++
@@ -1,7 +0,0 @@
-$(document).on('click', '#world-delete', function() {
-    return confirm("Are you sure you want to delete this world? It, and all its locations, will be gone forever!")
-});
-
-$(document).on('click', '#location-delete', function() {
-    return confirm("Are you sure you want to delete this location? It will be gone forever!")
-});
