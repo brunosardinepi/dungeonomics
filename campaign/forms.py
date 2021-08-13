9a6fc40b4917acac9a1b9d5ec6e2cb09317ecc62
gnowak <gn9012@gmail.com>
2021-08-13 15:58:26 -0500

    #153 -- consolidate forms

--- campaign/forms.py
+++ campaign/forms.py
@@ -3,38 +3,9 @@ from django import forms
 from django.contrib.auth.models import User
 
 
-#class CreateFormTemplate(forms.ModelForm):
-#    # Reference from CampaignCreate
-#    def get(self, request, *args, **kwargs):
-#        campaign = get_object_or_404(models.Campaign, pk=kwargs['campaign_pk'])
-#        if campaign.user == request.user:
-#            data = at_tagging(request)
-#            data['campaign'] = campaign
-#            data['form'] = forms.ChapterForm()
-#            data['chapter_number'] = utils.get_next_order(campaign)
-#            return render(request, 'campaign/chapter_form.html', data)
-#        raise Http404
-
-#    def post(self, request, *args, **kwargs):
-#        campaign = get_object_or_404(models.Campaign, pk=kwargs['campaign_pk'])
-#        if campaign.user == request.user:
-#            form = forms.ChapterForm(request.POST)
-#            if form.is_valid():
-#                chapter = form.save(commit=False)
-#                chapter.user = request.user
-#                chapter.campaign = campaign
-#                chapter.save()
-#                messages.add_message(request, messages.SUCCESS, "Chapter created")
-#                return HttpResponseRedirect(chapter.get_absolute_url())
-#        raise Http404
-
-
-class CampaignForm(forms.ModelForm):
+class FormTemplate(forms.ModelForm):
     class Meta:
-        model = models.Campaign
-        fields = [
-            'title',
-        ]
+        fields = ['title']
         widgets = {
             'title': forms.TextInput(attrs={'placeholder': 'Campaign title'}),
         }
@@ -43,37 +14,26 @@ class CampaignForm(forms.ModelForm):
         self.request = kwargs.pop('request')
         super().__init__(*args, **kwargs)
 
-class ChapterForm(forms.ModelForm):
+class CampaignForm(FormTemplate):
+    class Meta:
+        model = models.Campaign
+        fields = FormTemplate.Meta.fields
+
+class ChapterForm(FormTemplate):
     class Meta:
         model = models.Chapter
-        fields = [
-            'title',
+        fields = FormTemplate.Meta.fields + [
             'content',
             'order',
         ]
-        widgets = {
-            'title': forms.TextInput(attrs={'placeholder': 'Chapter title'}),
-        }
 
-    def __init__(self, *args, **kwargs):
-        self.request = kwargs.pop('request')
-        super().__init__(*args, **kwargs)
-
-class SectionForm(forms.ModelForm):
+class SectionForm(FormTemplate):
     class Meta:
         model = models.Section
-        fields = [
-            'title',
+        fields = FormTemplate.Meta.fields + [
             'content',
             'order',
         ]
-        widgets = {
-            'title': forms.TextInput(attrs={'placeholder': 'Section title'}),
-        }
-
-    def __init__(self, *args, **kwargs):
-        self.request = kwargs.pop('request')
-        super().__init__(*args, **kwargs)
 
 class ImportCampaignForm(forms.ModelForm):
     class Meta:
