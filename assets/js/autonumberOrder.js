a22a18aa51a29e576c062ebef56adbefd130ec27
gnowak <gn9012@gmail.com>
2021-08-16 07:11:19 -0500

    #153 -- fix autonumber on campaign

--- assets/js/autonumberOrder.js
+++ assets/js/autonumberOrder.js
@@ -2,7 +2,6 @@ $("#autonumber").on('click', function(event) {
   event.preventDefault();
   var count = 1;
   $(".autonumber-order").each(function() {
-    console.log($(this).val());
     $(this).val(count);
     count++;
   });
