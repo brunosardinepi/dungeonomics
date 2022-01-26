cc84730762e8fe9604b85af780b0e7a5dfb2fe2f
gnowak <gn9012@gmail.com>
2022-01-26 10:14:25 -0600

    hello world completed

--- dungeonomicsdrf/urls.py
+++ dungeonomicsdrf/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from dungeonomicsdrf import environ
+from dungeonomicsdrf.views import HelloWorldView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -13,4 +14,6 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+    path('hello/', HelloWorldView.as_view()),
 ]
