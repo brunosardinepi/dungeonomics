c6a8d26589f193feaa8ac12ee1dab730be2cfaee
gnowak <gn9012@gmail.com>
2022-02-18 08:30:25 -0600

    add ordering

--- resources/models.py
+++ resources/models.py
@@ -6,6 +6,9 @@ class Resource(models.Model):
     name = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    class Meta:
+        ordering = ['name']
+
     def get_absolute_url(self):
         from django.urls import reverse
         return reverse('resources:resource_detail', kwargs={'pk': self.pk})
