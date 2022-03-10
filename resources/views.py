f4854c1b7b8b137a2be6c30aeb5ac652eab9eaf9
gnowak <gn9012@gmail.com>
2022-03-10 14:23:48 -0600

    delete resource group if no resources are left

--- resources/views.py
+++ resources/views.py
@@ -70,6 +70,14 @@ class ResourceDelete(generics.DestroyAPIView):
     def get_queryset(self):
         return models.Resource.objects.filter(user=self.request.user)
 
+    def perform_destroy(self, instance):
+        # If this was the last resource in a resource group,
+        # delete the resource group.
+        for group in instance.resourcegroup_set.all():
+            if not group.resources.all().exclude(pk=instance.pk):
+                group.delete()
+        super().perform_destroy(instance)
+
 class ResourceDetail(generics.RetrieveAPIView):
     serializer_class = serializers.ResourceSerializer
     permission_classes = [IsAuthenticated]
