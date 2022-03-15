eeacef8775cedc9d347ee60e540258c34dad665e
gnowak <gn9012@gmail.com>
2022-03-15 13:31:32 -0500

    add email to user creation

--- dungeonomicsdrf/serializers.py
+++ dungeonomicsdrf/serializers.py
@@ -10,6 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
+            email=validated_data['username'],
             password=validated_data['password'],
         )
         return user
